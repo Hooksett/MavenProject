cf2246a7295d4a437873630e9149ac65885628c8
Hooksett <39606299+Hooksett@users.noreply.github.com>
2018-05-24 20:21:24 -0400

    Add files via upload

---
+++ MyApplication/src/test/java/Test1.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	
+		@Test
+		public void mavenTest() {
+		WebDriver driver = new ChromeDriver();
+		}
+	}
+
+
